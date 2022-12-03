c3d15ee401f5c2884ac5df8c87b2a1897796266d
Brandon Ros <brandonros1@gmail.com>
2022-12-03 15:00:16 -0500

    you can't ignore offset/len?

--- app/src/main/java/com/clusterrr/usbserialtelnetserver/TcpClientThread.java
+++ app/src/main/java/com/clusterrr/usbserialtelnetserver/TcpClientThread.java
@@ -50,10 +50,10 @@ public class TcpClientThread extends Thread {
         write(data, 0, data.length);
     }
 
-    public String byteArrayToHexString(byte[] bytes) {
+    public String byteArrayToHexString(byte[] bytes, int offset, int len) {
         char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-        char[] hexChars = new char[bytes.length * 2];
-        for (int j = 0; j < bytes.length; j++) {
+        char[] hexChars = new char[len * 2];
+        for (int j = offset; j < len; j++) {
           int v = bytes[j] & 0xFF;
           hexChars[j * 2] = HEX_ARRAY[v >>> 4];
           hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
@@ -65,7 +65,7 @@ public class TcpClientThread extends Thread {
         if (mDataOutputStream == null)
             return;
         // TODO: convert data to string or send binary message on websocket?
-        mWebSocketService.sendMessage(byteArrayToHexString(data));
+        mWebSocketService.sendMessage(byteArrayToHexString(data, offset, len));
     }
 
     public void close() {
